481859350eef2b365b960f2526dab31b5014146c
Grisha <grisha@Apples-MacBook-Air.local>
2021-08-12 13:14:44 +0400

    upadtes for second day

--- TestZevit/TestZevit/Program.cs
+++ TestZevit/TestZevit/Program.cs
@@ -23,12 +23,29 @@ namespace TestZevit
             // Task7(); // Secret word to ******
             // Task8(); // To upperCase each inputed char
             // Task9(); // +1 to each element
+            // ArrayTask2(); // Sum of array
+            // Arraytask3(); // Duplicate
+            // ArrayTask4(); // is Equal arrays or not?
+            // ArrayTask5(); // rectangle  
+            // SwapTask(); //Swap
+            // PrintTringle(); // printing tringle
+            // Factorial(); // n!, n^m, sqrt(n)
+
+            Console.WriteLine("Please input number to count fac:");
+            var fctr = Console.ReadLine();
+            
+
+            //for (int i = 0; i < fac; i++)
+            //{
+
+            //}
+
+
 
 
 
         }
-
-        static void FirstTask()
+        public static void FirstTask()
         {
             // First input
             double firstNum;
@@ -80,7 +97,7 @@ namespace TestZevit
                 }
             }
         }
-        static void SecondTask()
+        public static void SecondTask()
         {
             Console.WriteLine(" Please input Decimal number");
             int myNum;
@@ -279,6 +296,156 @@ namespace TestZevit
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.Write(arr[i]);
                 Console.ResetColor();
+            }
+
+            Console.ReadLine();
+        }
+
+
+        public static void ArrayTask2()
+        {
+            Console.WriteLine(" Please input array's length");
+            var lgth = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[lgth];
+            Console.WriteLine(" Please input array's members:");
+            var sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                sum += arr[i];
+            }
+            Console.WriteLine($" The sum of array's members is: {sum}");
+
+            Console.ReadLine();
+        }
+
+
+        public static void Arraytask3()
+        {
+            int i, j, num, count = 0;
+
+            Console.WriteLine(" Please input array's length: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            var arr = new int[num];
+
+            Console.WriteLine(" Please input elements in the array: ");
+            for (i = 0; i < num; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (i = 0; i < num; i++)
+            {
+                for (j = i + 1; j < num; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("\n Total number of duplicate elements in array is:" + count);
+            Console.ReadLine();
+        }
+        public static void ArrayTask4()
+        {
+            Console.WriteLine(" Please input array's length");
+            var arr1 = Convert.ToInt32(Console.ReadLine());
+            int[] firstArr = new int[arr1];
+            Console.WriteLine(" Please input array's members:");
+
+            for (int i = 0; i < firstArr.Length; i++)
+            {
+                firstArr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine(" Please input second arrays length");
+            var arr2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Please input array's members:");
+            int[] secondArr = new int[arr2];
+            for (int j = 0; j < secondArr.Length; j++)
+            {
+                secondArr[j] = Convert.ToInt32(Console.ReadLine());
+            }
+            if (arr1 != arr2)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Arrays are not Equal !");
+                Console.ResetColor();
+            }
+            if (firstArr.Length == secondArr.Length)
+            {
+                for (int i = 0; i < secondArr.Length; i++)
+                {
+                    if (firstArr[i] == secondArr[i])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n not equal!!!");
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n not equal 'Length'");
+            }
+
+            Console.ReadLine();
+
+        }
+        public static void ArrayTask5()
+        {
+            Console.Write("Height: ");
+            var height = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Width: ");
+            var width = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Char: ");
+            var symbol = Convert.ToChar(Console.ReadLine());
+            var rectangle = new char[height, width];
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    rectangle[i, j] = symbol;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(rectangle[i, j]);
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+        public static void SwapTask()
+        {
+            Console.WriteLine("Please input first number:");
+            var a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please input second number:");
+            var b = Convert.ToDouble(Console.ReadLine()); ;
+            Console.WriteLine($"Before -  a:{a}, b = {b}");
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.Write($"After - a:{a}, b = {b}");
+            Console.ReadLine();
+        }
+        public static void PrintTringle()
+        {
+            Console.WriteLine("Please insert tringle's hight:");
+            var hight = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Char: ");
+            var symbol = Convert.ToChar(Console.ReadLine());
+
+            for (int i = 0; i <= hight; i++)
+            {
+                var tringle = new string(symbol, i);
+                var result = tringle.PadLeft(hight, '-');
+                var result2 = tringle.PadRight(hight, '-');
+                Console.WriteLine(result + "*" + result2);
             }
 
             Console.ReadLine();
